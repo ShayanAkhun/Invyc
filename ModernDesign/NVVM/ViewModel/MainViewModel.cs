7c42fa1f53ff6226d45e6b80b4683921c9668f04
ShayanAkhun <76954640+ShayanAkhun@users.noreply.github.com>
2024-12-29 05:11:22 +0500

    added image and view models

---
+++ ModernDesign/NVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModernDesign.core;
+
+namespace ModernDesign.NVVM.ViewModel
+{
+    class MainViewModel : ObserverableObject
+    {
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; }
+        }
+
+
+
+
+
+
+        public MainViewModel()
+        {
+
+        }
+
+    }
+}
